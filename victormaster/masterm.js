a07ed7f27bc17571aac889ad23e6ec1a59377475
Daniel Fryar <daniel.fryar@gmail.com>
2016-11-22 19:23:06 -0600

    colors array update

--- victormaster/masterm.js
+++ victormaster/masterm.js
@@ -1,13 +1,12 @@
 
 
-var playerCode = []; //holds player's code
-var computerGuess = []; //holds computer's guesses
+var playerCode; //holds player's code
+var computerGuess; //holds computer's guesses
 
-var randomColors = ["yellow", "green","blue","red","purple","orange"]
-var spaces
+var randomColors = ["yellow", "green","blue","red","purple","orange"];
+var spaces;
 
 
-for (var i = 0; i < randomColor.length; i++){
-cosole.log(randomColors[i]);
-
+for (var i = 0; i < randomColors.length; i++){
+  console.log(randomColors[i]);
 }
