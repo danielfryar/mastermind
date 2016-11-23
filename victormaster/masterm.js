af90298c77cf6122c698c78f03b6166060d4baf3
Victor <victorgdl89@gmail.com>
2016-11-22 21:06:59 -0600

    css and js functions update

--- victormaster/masterm.js
+++ victormaster/masterm.js
@@ -1,12 +1,31 @@
 
-
 var playerCode; //holds player's code
 var computerGuess; //holds computer's guesses
 
-var randomColors = ["yellow", "green","blue","red","purple","orange"];
+var randomColors = ["yellow", "green", "blue", "red", "purple", "orange"];
 var spaces;
 
+function randomColors() {
+    for (var i = 0; i < randomColors.length; i++) {
+        console.log(randomColors[i]);
+				return Math.random(i);
+    }
+	}
 
-for (var i = 0; i < randomColors.length; i++){
-  console.log(randomColors[i]);
-}
+    function startGame() {
+
+        spaces = documnt.getElementById('colorSelect').value.parseInt();
+        for (var i = 0; i < spaces; i++) {
+            console.log(spaces);
+            answer.push(getRandomColor());
+            putSelector(i);
+        }
+    }
+
+		function colourFunction() {
+		var myselect = document.getElementById("select1"),
+		colour = myselect.options[myselect.selectedIndex].className;
+		myselect.className = colour;
+		myselect.blur(); //This just unselects the select list without having to click
+		somewhere else on the page
+		}
