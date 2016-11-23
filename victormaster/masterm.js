fc22b9fab5170f90b1fd02318cc2f7ad6223902c
Victor <victorgdl89@gmail.com>
2016-11-23 17:51:27 -0600

    html update 2.5

--- victormaster/masterm.js
+++ victormaster/masterm.js
@@ -1,24 +1,30 @@
 
 var playerCode; //holds player's code
-var computerGuess; //holds computer's guesses
 
 var randomColors = ["yellow", "green", "blue", "red", "purple", "orange"];
+var computerGuess; //holds computer's guesses
 var spaces;
 
-function rdmClrSelec() {
-    for (var i = 0; i < randomColors.length; i++) {
-
-				return Math.random(randomColors());
-    }
-		console.log(rdmClrSelec());
-	}
+// function rdmClrSelec() {
+//     for (var i = 0; i < randomColors.length; i++) {
+//         // computerGues
+// 				return Math.random(rdmClrSelec());
+//     }
+// 		console.log(rdmClrSelec());
+// 	}
 
     function startGame() {
 
-        spaces = document.getElementById('colorSelect').value.parseInt();
+        spaces = document.getElementById('select1').value.parseInt;
         for (var i = 0; i < spaces; i++) {
             console.log(spaces);
-            answer.push(randomColors());
+            answer.push(randomColors);
             putSelector(i);
         }
     }
+var computerGuess = function(){
+var comRanSele = randomColors[Math.floor(Math.random() * 6)];
+return comRanSele;
+	console.log(computerGuess());
+}
+computerGuess()
