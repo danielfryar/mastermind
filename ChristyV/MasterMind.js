bb693ed8e28c62de369625dc866f00801663d54b
ChristVolo1 <christy.volosin@gmail.com>
2016-11-22 20:51:06 -0600

    Updated JS and added CSS

--- ChristyV/MasterMind.js
+++ ChristyV/MasterMind.js
@@ -1,3 +1,16 @@
-var spaces;
-var colors =["yellow","green","blue","red","purple","orange"];
-var guesses,answers;
+var numSpaces;
+var colors = ["yellow", "green", "blue", "red", "purple", "orange"];
+var guesses, answers;
+
+
+for (var i = 0; i < guesses.length; i++) {
+    for (var i = 0; j < answers.length; j++) {
+        if (guesses[i] == answers[j]);
+        if (i === j) response += pico;
+    } else response += "fermi";
+  }
+
+if (response==='') response = 'bagels';
+
+return response;
+}
