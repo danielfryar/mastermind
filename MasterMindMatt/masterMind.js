a303cdb9d91daa3e5c632220fb53235c1b333955
Matthew Wegenke <matthew.wegenke@gmail.com>
2016-11-22 18:30:59 -0600

    1830 11.22.2016

--- MasterMindMatt/masterMind.js
+++ MasterMindMatt/masterMind.js
@@ -2,24 +2,41 @@ var spaces = document.getElementById('spaces').value;
 var gameBoard = document.getElementById("gameBoard");
 var nextRow = gameBoard.insertRow();
 var nextCell = nextRow.insertCell();
-var computerChoice1, computerChoice2, computerChoice3, computerChoice4, computerChoice5, computerChoice6 ;
+var computerChoice1, computerChoice2, computerChoice3, computerChoice4, computerChoice5, computerChoice6;
 var userGuess1, userGuess2, userGuess3, userGuess4, userGuess5, userGuess6;
-var answers = [computerChoice1, computerChoice2, computerChoice3, computerChoice4, computerChoice5, computerChoice6];
-var guesses = [userGuess1, userGuess2, userGuess3, userGuess4, userGuess5, userGuess6];
+// var answers = [computerChoice1, computerChoice2, computerChoice3, computerChoice4, computerChoice5, computerChoice6];
+// var guesses = [userGuess1, userGuess2, userGuess3, userGuess4, userGuess5, userGuess6];
 
-function getRandomForComputer (){
-  return Math.floor(Math.random() * 10);
+function compareCodeVsBreaker(guesses, answers){
+  var answerKey = '';
+
+  for (var i = 0; i <= guesses.length; i++){
+    for (var j = 0; i <= answers.lenght; j++)
+    if (guesses[i] == ansers[j]){
+      if (i === j) answerKey += 'black';
+      else answerKey += 'white'
+    }
+  }
+}
+
+function getRandomForComputer(){
+  return Math.random();
+}
+
+function startGame(){
+    computerChoice1 = getRandomForComputer();
+    computerChoice2 = getRandomForComputer();
+    computerChoice3 = getRandomForComputer();
+    computerChoice4 = getRandomForComputer();
+    computerChoice5 = getRandomForComputer();
+    computerChoice6 = getRandomForComputer();
 }
 
 document.getElementById('startGame').addEventListener('click', function(){
-  console.log(spaces);
+  startGame();
+  console.log(answers);
+  document.getElementById('startGame').disabled = true;
 });
-
-function startGame(){
-  do{
-
-  }
-}
 // NOTE: INFO FOR REFRENCE
 // i = value 1, yellow
 // j = value 2, green
