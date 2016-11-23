8c6a1e13a9affdf6b9c161a7823c352497d7c07b
Victor <victorgdl89@gmail.com>
2016-11-22 18:59:22 -0600

    js radom colors update

--- victormaster/masterm.js
+++ victormaster/masterm.js
@@ -1,3 +1,9 @@
 
-var randomColor
+var randomColors = ["yellow", "green","blue","red","purple","orange"]
 var spaces
+
+
+for (var i = 0; i < randomColor.length; i++){
+cosole.log(randomColors[i]);
+
+}
