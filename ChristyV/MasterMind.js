f994452bd11b08cf9bbef793dda9954a75e88634
ChristVolo1 <christy.volosin@gmail.com>
2016-11-23 21:01:53 -0600

    added some JQuery

--- ChristyV/MasterMind.js
+++ ChristyV/MasterMind.js
@@ -3,18 +3,20 @@ var colors = ["yellow", "green", "blue", "red", "purple", "orange"];
 var guesses, answers;
 
 $(document).ready(function() {
-
+$('.start').on('click',function(){
+  $("#Select Color").slideToggle(1000);
+});
 function compareGuess() {
 for (var i = 0; i < guesses.length; i++) {
     for (var j = 0; j < answers.length; j++) {
         if (guesses[i] == answers[j]);
         if (i === j) response += "pico";
     }
-    else response += "fermi";
+    //else response += "fermi";
   }
 
 if (response==='') response = 'bagels';
 
 return response;
 }
-}
+});
