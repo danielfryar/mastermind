7472403b8d043637c0cbdbcb99668426abdf6193
ChristVolo1 <christy.volosin@gmail.com>
2016-11-23 20:54:46 -0600

    Just pushing my code, for what it's worth.

--- ChristyV/MasterMind.js
+++ ChristyV/MasterMind.js
@@ -2,13 +2,15 @@ var numSpaces;
 var colors = ["yellow", "green", "blue", "red", "purple", "orange"];
 var guesses, answers;
 
+$(document).ready(function() {
 
 function compareGuess() {
 for (var i = 0; i < guesses.length; i++) {
-    for (var i = 0; j < answers.length; j++) {
+    for (var j = 0; j < answers.length; j++) {
         if (guesses[i] == answers[j]);
         if (i === j) response += "pico";
-    } else response += "fermi";
+    }
+    else response += "fermi";
   }
 
 if (response==='') response = 'bagels';
