46094ea4ddee8e28904cb2f0751c3677a348285e
Matthew Wegenke <matthew.wegenke@gmail.com>
2016-11-28 22:49:47 -0600

    11.28.2016 @  2249

--- MasterMindMatt/masterMind.js
+++ MasterMindMatt/masterMind.js
@@ -1,63 +1,65 @@
-$(document).ready(function(){
-var colors = ["yellow", 'green','blue','red','purple','orange'];
-var difficultChoice;
-var gameBoard; /*= document.getElementById("gameBoard");*/
-var nextRow;/* = gameBoard.insertRow();*/
-var nextCell;/* = nextRow.insertCell();*/
-var answers = [];
-var guesses = [];
-var guessNumber = [];
-var answerKey;
-var compChoice;
+$(document).ready(function() {
+    var colors = ["yellow", 'green', 'blue', 'red', 'purple', 'orange'];
+    var difficultChoice;
+    var gameBoard; /*= document.getElementById("gameBoard");*/
+    var nextRow; /* = gameBoard.insertRow();*/
+    var nextCell; /* = nextRow.insertCell();*/
+    var answers = [];
+    var guesses = [];
+    var guessNumber = [];
+    var answerKey;
+    var compChoice;
 
-function numberOfGuesses(){
-  for (var i = 0; i < difficultChoice; i++){
-  document.getElementById('selectUser'+ i).style.visibility = "visible";
-}}
+    function numberOfGuesses() {
+        for (var i = 0; i < difficultChoice; i++) {
+            document.getElementById('selectUser' + i).style.visibility = "visible";
+        }
+    }
 
-function codeBreaker(guesses, answers){
-  answerCode = [];
-  for (var i = 0; i < guesses.length; i++){
-    for (var j = 0; j < answers.length; j++){
-      if (guesses[i] == answers[j]){
-        if (i === j) answerCode.push('black');
-        else answerCode.push('white');
-      }
-    }console.log(answerCode);
-  }
-  answerKey = answerCode;
-}
+    function codeBreaker(guesses, answers) {
+        answerCode = [];
+        for (var i = 0; i < guesses.length; i++) {
+            for (var j = 0; j < answers.length; j++) {
+                if (guesses[i] == answers[j]) {
+                    if (i === j) answerCode.push('black');
+                    else answerCode.push('white');
+                }
+            }
+            console.log(answerCode);
+        }
+        answerKey = answerCode;
+    }
 
 
-function getRandomNumber(){
-  return Math.floor(Math.random() * 10);
-}
+    function getRandomNumber() {
+        return Math.floor(Math.random() * 10);
+    }
 
-function colorSelector(){
-  for (var i = 0; i < difficultChoice; i++){
-    do{
-      var answer = getRandomNumber();
-    }while (answer == undefined || answer > 5);
-  compChoice = colors[answer];
-  answers.push(compChoice);
-  }
-}
+    function colorSelector() {
+        for (var i = 0; i < difficultChoice; i++) {
+            do {
+                var answer = getRandomNumber();
+            } while (answer == undefined || answer > 5);
+            compChoice = colors[answer];
+            answers.push(compChoice);
+        }
+    }
 
-function startGame(){
-  difficultChoice = Number.parseInt(document.getElementById('difficulty').value);
-  numberOfGuesses();
-}
+    function startGame() {
+        difficultChoice = Number.parseInt(document.getElementById('difficulty').value);
+        numberOfGuesses();
+    }
 
-$("#startGame").click(function(){
-  startGame();
-  colorSelector();
-  document.getElementById('guessButton').disabled = false;
-  document.getElementById('startGame').disabled = true;
-  console.log(answers);
-});
+    $("#startGame").click(function() {
+        startGame();
+        colorSelector();
+        document.getElementById('guessButton').disabled = false;
+        document.getElementById('startGame').disabled = true;
+        console.log(answers);
+    });
 
-$('#guessButton').click(function(e){
-  codeBreaker(guesses, answers);
-});
+    $('#guessButton').click(function(e) {
+        codeBreaker(guesses, answers);
+    });
 
 });
